0375ff4c5df9f63c34221b72e44ad7cc6db7e7bf
nkzttt <free.nkzt@gmail.com>
2021-06-08 13:43:31 +0900

    updated webpack.

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,34 @@
 const path = require('path');
 
-module.exports = {
-  entry: path.join(__dirname, 'src/js/index'),
-  output: {
-    path: path.join(__dirname, 'dist/js'),
-    filename: 'app.bundle.js'
-  },
-  resolve: {
-    extensions: ['.ts', '.js', '.json']
-  },
-  module: {
-    rules: [{
-      test: /\.(ts)|(js)$/,
-      exclude: /node_modules/,
-      loader: 'babel-loader'
-    }]
-  },
-  mode: 'development',
-  devtool: 'inline-source-map'
+module.exports = (env, argv) => {
+  const mode = argv.mode || 'development';
+  const isProduction = mode === 'production';
+  return {
+    entry: path.join(__dirname, 'src/js/index'),
+    output: {
+      path: path.join(__dirname, 'dist/js'),
+      filename: 'app.bundle.js'
+    },
+    resolve: {
+      extensions: ['.ts', '.js', '.json'],
+      fallback: {
+        "path": require.resolve("path-browserify"),
+        "url": require.resolve("url")
+      }
+    },
+    module: {
+      rules: [{
+        test: /\.(ts)|(js)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }]
+    },
+    mode,
+    devtool: isProduction ? undefined : 'inline-source-map'
+  };
 };
